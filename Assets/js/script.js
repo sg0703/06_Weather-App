3a14e5ed69ac6093525e1660493141cca46fe55e
sg0703 <samgates@Sams-MacBook-Pro.local>
2021-03-12 14:57:22 -0500

    Added forecast sectionand styling, created default start page, fixed bugs with changing search query, set up empty functions in prep for saving past searches

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -9,17 +9,22 @@ $('#search_btn').on('click', function(e) {
     console.log("YAY");
     e.preventDefault();
     
-    // add some kind of input validation later
-    //showResults($('#city').val());
+    showResults($('#city').val());
 });
 
+displayStart();
 
-showResults('Raleigh');
+// display initial start page
+function displayStart() {
+    $('#forecast_section').attr("style","display: none");
+    $('#city_name').html("Welcome to the very exciting Weather App!");
+    $('#city_content').html("Please use the search bar to the left to continue.");
+}
 
+// show data user requested (relies on functions below)
 function showResults(city) {
     var cityLat = '';
     var cityLong = '';
-    var cityName = city;
 
     // API key for sam.j.gates@gmail.com
     var apiKey = '2d1455849cad8d73b4d9605338104f0f';
@@ -29,12 +34,12 @@ function showResults(city) {
     // use promise chain to 1. get coordinates using geo decode API, THEN 2. get all weather data needed for project from one call API 
     fetch(getCoordinatesUrl, {mode: 'cors'})
         .then(function (response) {
-        // Check the console first to see the response.status
-        console.log(response.status);
-        if(response.status === 404) {
-        //error response!
-        }
-        return response.json();
+            // Check the console first to see the response.status
+            console.log(response.status);
+            if(response.status === 404) {
+                //error response!
+            }
+            return response.json();
         })    
         // use coordinates to make a second API call to get all weather data
         .then(function (data) {
@@ -51,9 +56,10 @@ function showResults(city) {
             }
             return response.json();
         })
-        .then(function(data) {
-            displayToday(data,cityName);
-            displayForecast(data,cityName);
+        // finally, send weather data to page
+        .then(function(data) { 
+            displayToday(data,city); // display current weather info to page
+            displayForecast(data,city); // display forecase to page
         });
 }
 
@@ -66,29 +72,88 @@ function displayToday(data,city) {
     var humidity = data.current.humidity;
     var wind = data.current.wind_speed;
 
+    // change styling of UV based on scale
     var UV = data.current.uvi;
+    var styledUV = ''; // string with styling for different codes
+
+    if (UV < 3) {
+        styledUV = '<span style=\'background-color: green; padding: 5px;\'>' + UV;
+    }
+    else if (UV > 3 && UV < 6) {
+        styledUV = '<span style=\'background-color: yellow; padding: 5px;\'>' + UV;
+    }
+    else {
+        styledUV = '<span style=\'background-color: red; padding: 5px;\'>' + UV;
+    }
 
     var weatherIconUrl = 'http://openweathermap.org/img/wn/' +  data.current.weather[0].icon + '.png';
+
+    var weatherAlt = data.current.weather[0].description;
 
     // still have to apply icon for weather, see if UV good or bad
 
     // display date/time
     var time = moment().format('MM/DD/YY');
 
-    // display city name at top of content section
-    $('#city_name').html(city + " on " + time + " <img src=" + weatherIconUrl + ">");
+    // display city name at top of content section, along with image 
+    var imageEl = document.createElement("img");
+    imageEl.setAttribute("src",weatherIconUrl);
+    imageEl.setAttribute("alt",weatherAlt);
+    
+    $('#city_name').html(city + " on " + time + " ");
+    $('#city_name').append(imageEl);
 
+    // display list of current conditions extracted from API data
     $('#city_content').html('<ul>' +
     '<li>Temperature: ' + temperature + ' degrees</li>' +
     '<li>Humidity: ' + humidity + '%</li>' +
     '<li>Wind speed: ' + wind + ' MPH</li>' +
-    '<li>UV Index: <span style=\'background-color: red; padding: 5px;\'>' + UV + '</span></li>' +
+    '<li>UV Index: ' + styledUV + '</span></li>' +
     '</ul>');
 }
 
 // display 5 day forecast for weather at location
 function displayForecast(data) {
-    console.log(data);
+    // reset entire container so data doesn't build on itself
+    $('#city_forecast').html('');
+    $('#forecast_section').attr("style","display: all");
+
+    // display 5 day forecast using API data
+    for (i = 1; i <= 5; i++) { 
+        // create containers, then add content below
+        var cardContainer = document.createElement("div");
+        cardContainer.setAttribute("class","card col-md-2 p-0 mx-2");
+
+        var cardHeader = document.createElement("div");
+        cardHeader.setAttribute("class","card-header p-1");
+
+        // add date to header
+        cardHeader.innerHTML = "<h5>" + getDate(data.daily[i].dt) + "</h5>";
+
+        // create card body
+        var cardBody = document.createElement("div");
+        cardBody.setAttribute("class","card-body p-2");
+
+        // build src link for image with alt descr
+        var weatherIconUrl = 'http://openweathermap.org/img/wn/' +  data.daily[i].weather[0].icon + '.png'; 
+        var weatherIconDescr = data.daily[0].weather[0].description;
+        var weatherIcon = document.createElement("img");
+        weatherIcon.setAttribute("src",weatherIconUrl);
+        weatherIcon.setAttribute("alt",weatherIcon);
+        cardBody.append(weatherIcon);
+
+        // create list for other attributes, add to cardBody
+        var forecastList = document.createElement("ul");
+        forecastList.innerHTML = 
+            "<li>Temp: " + data.daily[i].temp.day + "&deg;F</li>" +
+            "<li>Humidity: " + data.daily[i].humidity + "%</li>";
+        cardBody.append(forecastList);
+
+        // build card, add to page
+        cardContainer.append(cardHeader);
+        cardContainer.append(cardBody);
+        $('#city_forecast').append(cardContainer);
+    }
 }
 
 // stores past searches in localstorage
@@ -106,3 +171,6 @@ function clearPastSearches() {
     
 }
 
+function getDate(timestamp) {
+    return moment.unix(timestamp).format('MM/DD/YY');
+}
